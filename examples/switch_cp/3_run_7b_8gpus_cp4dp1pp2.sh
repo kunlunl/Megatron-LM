3135152631771ff6d846a95273725a8876e4e8c0
kunlunl <kunlunl@nvidia.com>
2025-05-14 05:54:32 -0700

    Add script for cp2 and cp4

---
+++ examples/switch_cp/3_run_7b_8gpus_cp4dp1pp2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (c) 2024, Kuaishou Technology. All rights reserved.
+
+set -euo pipefail
+
+source ./llama-7b
+
+export SEQ_LENGTH=100000
+export GLOBAL_BATCH_SIZE=16
+
+export HOSTFILE=
+export MASTER_ADDR=127.0.0.1
+export NUM_GPUS=8
+
+export TP=1
+export CP=4
+export ALL_CP="4"
+export PP=2
+export CKPT=full
+export OFFLOAD_ALPHA=0.0
+export NUM_LAYERS=32
+
+./sft_llama_no_vpp.sh
